c932431bc556815c5b6487fc695d7e00e2443f8f
Ariel Oak <codestothestars@yahoo.com>
2020-08-14 02:39:14 -0400

    Move demo app resources to app folder
    Organization.

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './App';
-import './index.css';
+import { App } from './app';
 import * as serviceWorker from './serviceWorker';
+import './app/index.css';
 
 render(
   <React.StrictMode>
