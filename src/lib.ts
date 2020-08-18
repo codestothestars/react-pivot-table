f89bf08550deafca05b9bfaa8b899ee0009a101c
Ariel Oak <codestothestars@yahoo.com>
2020-08-17 21:32:13 -0400

    Export Aggregator type in public API
    Users might want to reference it, as it is already implicitly exposed
    through the API.

--- src/lib.ts
+++ src/lib.ts
@@ -1,3 +1,4 @@
+export type { Aggregator } from './aggregation';
 export { aggregators } from './aggregation';
 export type { Dimension } from './dimensions';
 export type { TitleNames } from './head';
